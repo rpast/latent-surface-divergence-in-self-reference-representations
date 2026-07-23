43c2417ebcdb911622478e354370ceaa32d0bafe
Rafal Pastuszak <rpast@protonmail.com>
2026-07-23 11:56:12 +0000

    project env setup

--- run_env.sh
+++ run_env.sh
@@ -1,6 +1,9 @@
 source /workspace/nla_infer_010726/venv/bin/activate
+
+git config --global user.email "rpast@protonmail.com"
+git config --global user.name "rpast"
+git config --global --add safe.directory /workspace/nla_infer_010726
+
+export JUPYTER_PATH=/workspace/jupyter
 export HF_HOME=/workspace/nla_infer_010726/hf
 export SNAP=$(ls -d $HF_HOME/hub/models--kitft--nla-qwen2.5-7b-L20-av/snapshots/*/ 2>/dev/null)
-git config --global user.email "rpast@protonmail.com"
-git config --global user.name "Rafal Pastuszak"
-git config --global --add safe.directory /workspace/nla_infer_010726
